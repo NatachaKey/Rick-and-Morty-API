4bef7c98ce422b28724952cca40d886cdc1e51a5
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-12-21 20:34:45 +0100

    Update CharacterCard.js

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -3,9 +3,9 @@ function CharacterCard(props) {
 
   return (<div>
     <img src={character.image} width= "150px" alt="caracter"/>
-     <p>Name:{character.name}</p>
-     <p>Gender:{character.gender}</p>
-     <p>Species:{character.species}</p>
+     <p>Name: {character.name}</p>
+     <p>Gender: {character.gender}</p>
+     <p>Species: {character.species}</p>
   </div>);
 }
 
